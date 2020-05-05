c05b68e3704f76cff200a2ba7e28fe7e3cb4ac1e
Frank <fefe@Franks-MacBook-Pro.local>
2020-05-05 12:43:17 +0200

    ff

--- pravitko-vizs/prod.py
+++ pravitko-vizs/prod.py
@@ -1,20 +1,21 @@
-# Import OpenCV
+#Import OpenCV and Numpy libraries
 import numpy as np
 import cv2
 
-#Loading undistortion matrix
+#Loading undistortion matrices gathered from calib.py script
 
 mtx = np.loadtxt('./calibration/mtx.txt')
 dist = np.loadtxt('./calibration/dist.txt')
 tvecs = open('./calibration/tvecs.txt')
 rvecs = open('./calibration/rvecs.txt')
 
+#Scale was measured directly on site with length measurement device and calculated from undistorted photo
 #Pixels per milimeter
-
-mierka = 1.35526315789
+scale = 1.35526315789
 
 #Functions
 
+#Helper function for rotateImage()
 def getTranslationMatrix2d(dx, dy):
     """
     Returns a numpy affine transformation matrix for a 2D translation of
@@ -22,7 +23,7 @@ def getTranslationMatrix2d(dx, dy):
     """
     return np.matrix([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
 
-
+#Function for rotating image with adequate framebuffer adjustment
 def rotateImage(image, angle):
     """
     Rotates the given image about it's centre
@@ -82,9 +83,9 @@ cap = cv2.VideoCapture('./videos/minus3.avi') # 0- Primary camera ,1- External c
 # Read the image
 ret, frame = cap.read()
 
-#Undistore
-h,  w = frame.shape[:2]
 
+#Undistore the image
+h,  w = frame.shape[:2]
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
 
@@ -95,37 +96,37 @@ while(1):
 
     dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
 
+    #Rotating image
     img = rotateImage(dst, 36.3)
+
+    #Preparing samples for visualization and analysis
     output = img[1700:2200,175:3980]
     img = img[1850:1860,225:3930]
 
+    #Values for proper visualization - calculated from output and img offset
     offsetY  = 150
     offsetX = 50
 
     h,  w = img.shape[:2]
 
-    mask = np.zeros(img.shape[0:2], dtype=np.uint8)
-    #Maska na rotovane 
-    points = np.array([[[0,1],[3705,1],[3705,49],[0,49]]])
-    #Maska na original
-    cv2.drawContours(mask, [points], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    img = cv2.bitwise_and(img,img,mask = mask)
-
-    #convert from BGR to HSV color space
+    #Convert from BGR to HSV color space
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #apply threshold
+    #Apply threshold 
     thresh = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)[1]
 
-    # find contours and get one with area about 180*35
-    # draw all contours in green and accepted ones in red
+    #Find contours
     contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
-    #area_thresh = 0
+    
+    #Specify area limits
     min_area = 0.05 * w * h
     max_area = 0.99 * w * h
+
     result = img.copy()
 
+    #Take contour only within specified area range
+    #Bound contour with straight rectangle and calculate distance from pixels
     for c in contours:
         area = cv2.contourArea(c)
         cv2.drawContours(result, [c], -1, (0, 255, 0), 1)
@@ -133,12 +134,12 @@ while(1):
                 x,y,w,h = cv2.boundingRect(c)
                 cv2.rectangle(output, (x + offsetX, y + offsetY), (x + offsetX + w, y + offsetY+ h), (255, 0,0), -1)
                 cv2.rectangle(output, (offsetX,offsetY), (x + offsetX,y + offsetY+h), (0, 0, 255), -1)
-                vzdialenost = 'Position: ' + str(int(x/mierka))
-                sirka = 'Width: ' + str(int(w/mierka))
-                tmp = vzdialenost + ' mm ' + sirka + ' mm'
+                distance = 'Position: ' + str(int(x/scale))
+                width = 'Width: ' + str(int(w/scale))
+                tmp = distance + ' mm ' + width + ' mm'
                 cv2.putText(output, str(tmp), (25,25), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
 
-    # show images
+    #Show images
     cv2.imshow("GRAY", gray)
     cv2.imshow("THRESH", thresh)
     cv2.imshow('RESULT', result)
